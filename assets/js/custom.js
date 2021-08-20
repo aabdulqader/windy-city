c543174562fd1c0e8058f05768634b32d0d0e669
shimul-aq <shimul.official7@gmail.com>
2021-08-21 02:16:44 +0600

    complete

---
+++ assets/js/custom.js
@@ -0,0 +1,57 @@
+$(document).ready(function () {
+
+
+
+
+    $('.popup-gallery').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        tLoading: 'Loading image #%curr%...',
+        mainClass: 'mfp-img-mobile',
+        gallery: {
+            enabled: true,
+            navigateByImgClick: true,
+            preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+        },
+        image: {
+            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+            titleSrc: function (item) {
+                return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
+            }
+        }
+    });
+
+
+
+
+    $('.owl-studio').owlCarousel({
+        loop: true,
+        dots: false,
+        nav: true,
+        navText: ["<i class='flaticon-left-arrow'>", "<i class='flaticon-right-arrow'>"],
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
+        }
+    })
+
+
+
+
+
+    $('.popup-youtube').magnificPopup({
+        disableOn: 700,
+        type: 'iframe',
+        mainClass: 'mfp-fade',
+        removalDelay: 160,
+        preloader: false,
+        fixedContentPos: false
+    });
+});
